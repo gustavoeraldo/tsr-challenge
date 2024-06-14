c35d8c0ef79eb2320ce4c3763f0b914cca6d51d9
Gustavo Eraldo <gustavo.silva@nlt.com.br>
2024-06-14 17:13:55 -0300

    refactor: Refactoring main.py to improve code organization and readability

--- main.py
+++ main.py
@@ -2,15 +2,33 @@ from fastapi import FastAPI, responses
 from fastapi.openapi.docs import get_swagger_ui_html
 
 from app.v1.config.settings import settings
+from app.v1.container import Container
+from app.v1.routes import routes as v1_routes
 
-app = FastAPI(
-    title="TSF API",
-    description="Beneficiary management",
-    version=settings.API_VERSION,
-    redoc_url=None,
-    docs_url="/",
-    openapi_url="/openapi.json",
-)
+
+def create_app() -> FastAPI:
+    container = Container()
+
+    db = container.db()
+    db.create_database()
+
+    _app = FastAPI(
+        title="TSF API",
+        description="Beneficiary management",
+        version=settings.API_VERSION,
+        redoc_url=None,
+        docs_url="/",
+        openapi_url="/openapi.json",
+    )
+
+    _app.container = container
+
+    return _app
+
+
+app = create_app()
+
+app.include_router(v1_routes, prefix="/v1")
 
 
 @app.get("/docs", include_in_schema=False)
