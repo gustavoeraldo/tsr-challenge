f6f55175f6b36ad57d9966e73811c53811bf14b9
Gustavo Eraldo <gustavo.silva@nlt.com.br>
2024-06-17 10:14:03 -0300

    feat: Add beneficiary endpoints and include them in the API router

--- app/v1/routes.py
+++ app/v1/routes.py
@@ -1,3 +1,9 @@
 from fastapi import APIRouter
 
-routes = APIRouter()
+from app.v1.entities.beneficiary.beneficiary_endpoints import (
+    router as beneficiary_router,
+)
+
+routes = APIRouter(tags=["Beneficiary"])
+
+routes.include_router(beneficiary_router)
