16dcade97c2711b27399ac280ee55857e5cbd9cd
Gustavo Eraldo <gustavo.silva@nlt.com.br>
2024-06-17 10:10:48 -0300

    feat: Add beneficiary schemas

--- app/v1/entities/beneficiary/schemas/beneficiary_schemas.py
+++ app/v1/entities/beneficiary/schemas/beneficiary_schemas.py
@@ -1,5 +1,6 @@
-from pydantic import Field, BaseModel
+from pydantic import Field, BaseModel, field_validator
 from typing import Optional
+import re
 
 from app.v1.entities.bank_account.schemas.bank_account_schema import (
     BankAccountBaseSchema,
@@ -19,4 +20,44 @@ class BeneficiaryBaseSchema(BaseModel):
     )
 
 
-class BeneficiaryFormSchema(BeneficiaryBaseSchema, BankAccountBaseSchema): ...
+class BeneficiaryFormSchema(BankAccountBaseSchema, BeneficiaryBaseSchema):
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "name": "João da Silva",
+                "email": "JOAO.DA.SILVA@GMAIL.COM",
+                "cpf_cnpj": "067.956.219-07",
+                "pix_key_type": "EMAIL",
+                "pix_key": "joao.da.silva@gmail.com",
+            }
+        }
+    }
+
+
+class BeneficiaryUpdatechema(BaseModel):
+    name: Optional[str] = None
+    email: Optional[str] = None
+    cpf_cnpj: Optional[str] = None
+    status: Optional[str] = None
+
+    @field_validator("email")
+    def validate_email(cls, value):
+        if value is not None:
+            if not re.match(r"^[A-Z0-9+_.-]+@[A-Z0-9.-]+$", value):
+                raise ValueError("Invalid email")
+        return value
+
+    @field_validator("cpf_cnpj")
+    def validate_cpf_cnpj(cls, value):
+        if value is not None:
+            if not re.match(
+                r"(^[0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2}$)|(^[0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2}$)",
+                value,
+            ):
+                raise ValueError("Invalid cpf_cnpj")
+        return value
+
+
+class BeneficiaryCreateInDBSchema(BeneficiaryBaseSchema):
+    status: str = "rascunho"
