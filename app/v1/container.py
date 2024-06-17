f5e7527485b80674b2b4c8ab503af3c2648aa672
Gustavo Eraldo <gustavo.silva@nlt.com.br>
2024-06-17 10:13:48 -0300

    feat: mapping beneficiary repositories and services in the application container

--- app/v1/container.py
+++ app/v1/container.py
@@ -3,8 +3,30 @@ from dependency_injector import containers, providers
 from .database.db import Database
 from .config.settings import settings
 
+from app.v1.entities.beneficiary.model.beneficiary_model import BeneficiaryModel
+from app.v1.entities.beneficiary.service.beneficiary_service import BeneficiaryService
+from app.v1.entities.beneficiary.repository.beneficiary_repository import (
+    BeneficiaryRepository,
+)
+
 
 class Container(containers.DeclarativeContainer):
+    config = providers.Configuration()
+    wiring_config = containers.WiringConfiguration(
+        modules=["app.v1.entities.beneficiary.service.beneficiary_service"],
+    )
+
     db = providers.Singleton(Database, database_url=settings.DATABASE_URL)
 
     settings = providers.Object(settings)
+
+    beneficiary_repository = providers.Factory(
+        BeneficiaryRepository,
+        model=BeneficiaryModel,
+        session_factory=db.provided.session,
+    )
+
+    beneficiary_service = providers.Factory(
+        BeneficiaryService,
+        repository=beneficiary_repository,
+    )
