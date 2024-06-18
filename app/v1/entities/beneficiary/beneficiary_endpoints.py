68c9e342b647090dba3a9062b1251620707b40e6
Gustavo Eraldo <gustavo.silva@nlt.com.br>
2024-06-18 06:16:04 -0300

    feat: Update beneficiary endpoints to include beneficiary update functionality

--- app/v1/entities/beneficiary/beneficiary_endpoints.py
+++ app/v1/entities/beneficiary/beneficiary_endpoints.py
@@ -1,14 +1,18 @@
 from fastapi import APIRouter, Depends, HTTPException, status
-from dependency_injector.wiring import inject
 
 from app.v1.container import Container
+from app.v1.entities.bank_account.bank_account_service import BankAccountService
 from app.v1.entities.beneficiary.service.beneficiary_service import BeneficiaryService
 from app.v1.entities.beneficiary.schemas.beneficiary_schemas import (
     BeneficiaryFormSchema,
-    BeneficiaryCreateInDBSchema,
+    BeneficiaryUpdateFormSchema,
 )
 
 router = APIRouter()
+
+
+def get_bank_account_service():
+    return Container.bank_account_service()
 
 
 def get_beneficiary_service():
@@ -19,13 +23,12 @@ def get_beneficiary_service():
     "/beneficiary",
     status_code=status.HTTP_201_CREATED,
 )
-@inject
 async def create_beneficiary(
     beneficiary: BeneficiaryFormSchema,
     service: BeneficiaryService = Depends(get_beneficiary_service),
 ):
     # Create a new beneficiary
-    new_beneficiary = await service.create_v2(beneficiary)
+    new_beneficiary = await service.create(beneficiary)
 
     # Send a request to validate the new beneficiary
     # service.validate_beneficiary(new_beneficiary)
@@ -40,13 +43,19 @@ async def get_beneficiary(
     return service.get(beneficiary_id)
 
 
-@router.patch("/beneficiary/{beneficiary_id}")
+@router.patch("/beneficiary/{beneficiary_id}", status_code=status.HTTP_200_OK)
 async def update_beneficiary(
     beneficiary_id: int,
-    beneficiary: BeneficiaryFormSchema,
+    data: BeneficiaryUpdateFormSchema,
     service: BeneficiaryService = Depends(get_beneficiary_service),
+    bank_account_service: BankAccountService = Depends(get_bank_account_service),
 ):
-    return service.update_by_id(beneficiary_id, beneficiary)
+    # Update the beneficiary
+    updated_beneficiary = await service.update_by_id(beneficiary_id, data)
+
+    # Update the beneficiary bank account
+    await bank_account_service.update_by_beneficiary_id(beneficiary_id, data)
+    return updated_beneficiary
 
 
 @router.delete("/beneficiary/{beneficiary_id}", status_code=status.HTTP_204_NO_CONTENT)
