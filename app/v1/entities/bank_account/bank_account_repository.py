6292ec2867cd2feaabf74047056470b078390f79
Gustavo Eraldo <gustavo.silva@nlt.com.br>
2024-06-17 23:25:23 -0300

    feat: Implement BankAccountRepository for managing bank account data

---
+++ app/v1/entities/bank_account/bank_account_repository.py
@@ -0,0 +1,33 @@
+from app.v1.core.repository import Repository, NotFoundError
+from app.v1.entities.bank_account.model.bank_account_model import BankAccountModel
+from app.v1.entities.bank_account.bank_account_schemas import (
+    BankAccountCreateInDB,
+    BankAccountUpdateInDB,
+)
+
+
+class NotFoundBankAccountError(NotFoundError):
+    entity_name: str = "Bank Account"
+
+
+class BankAccountRepository(
+    Repository[BankAccountModel, BankAccountCreateInDB, BankAccountUpdateInDB]
+):
+    def create(self, data: BankAccountCreateInDB) -> BankAccountModel:
+        return super().create(data)
+
+    def get_by_id(self, entity_id: int) -> BankAccountModel:
+        data = super().get_by_id(entity_id)
+
+        if not data:
+            raise NotFoundBankAccountError(entity_id)
+        return super().get_by_id(entity_id)
+
+    def update_by_id(
+        self, entity_id: int, data: BankAccountUpdateInDB
+    ) -> BankAccountModel:
+        return super().update_by_id(entity_id, data)
+
+    def delete_by_id(self, entity_id: int) -> None:
+        obj = self.get_by_id(entity_id)
+        return super().delete_by_id(obj)
