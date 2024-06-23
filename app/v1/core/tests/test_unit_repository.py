545177c550a8a452ea392085f032241b338d87b7
Gustavo Eraldo <gustavo.silva@nlt.com.br>
2024-06-23 17:24:20 -0300

    test: Add crud unit tests for Repository class in test_unit_repository.py

--- app/v1/core/tests/test_unit_repository.py
+++ app/v1/core/tests/test_unit_repository.py
@@ -1,9 +1,10 @@
 from unittest import TestCase
+from sqlalchemy import Column, Integer
 from sqlalchemy.orm import Session
-from unittest.mock import create_autospec
+from unittest.mock import create_autospec, patch
 from contextlib import AbstractContextManager
 from typing import Callable
-from sqlalchemy import Column, Integer
+from pydantic import BaseModel
 
 from app.v1.core.repository import Repository
 from app.v1.database.db import Base
@@ -13,6 +14,10 @@ class StubTestModel(Base):
     __tablename__ = "test_table"
 
     id = Column(Integer, primary_key=True)
+
+
+class StubTestModelUpdateSchema(BaseModel):
+    id: int
 
 
 class TestUnitRepository(TestCase):
@@ -29,3 +34,29 @@ class TestUnitRepository(TestCase):
     def test_repository_constructor(self):
         self.assertEqual(self.repository.model, self.model)
         self.assertEqual(self.repository.session_factory, self.mock_session_factory)
+
+    def test_repository_get_by_id(self):
+        self.repository.get_by_id(1)
+        self.mock_session_factory.assert_called_once()
+
+        # self.assertIsInstance(test_model_data, StubTestModel)
+
+    def test_get(self):
+        self.repository.get({"id": 1})
+        self.mock_session_factory.assert_called_once()
+
+    def test_create(self):
+        self.repository.create({"id": 1})
+        self.mock_session_factory.assert_called_once()
+
+    def test_delete_by_id(self):
+        self.repository.delete_by_id(1)
+        self.mock_session_factory.assert_called_once()
+
+    @patch("app.v1.core.repository.Repository.get_by_id")
+    def test_update_by_id(self, get_by_id_mock):
+        # Mock setup
+        get_by_id_mock.return_value = StubTestModel(id=1)
+
+        self.repository.update_by_id(1, StubTestModelUpdateSchema(id=1))
+        self.mock_session_factory.assert_called()
